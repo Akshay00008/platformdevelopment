a48e9161b1ba7b6d7e6d65f6f8657205747c365a
Akshay00008 <Akshay.sharma@algo8.ai>
2025-07-02 14:00:08 +0530

    mongo connection

--- utility/Files_upload_description.py
+++ utility/Files_upload_description.py
@@ -17,7 +17,7 @@ logging.basicConfig(level=logging.INFO)
 
 # MongoDB setup
 mongo_client = pymongo.MongoClient("mongodb://dev:N47309HxFWE2Ehc@35.209.224.122:27017")
-db = mongo_client["ChatbotDB"]
+db = mongo_client["ChatbotDB-DEV"]
 collection = db['files']
 
 # Load environment variables from .env file
@@ -69,7 +69,7 @@ def generate_openai_output(text):
 # Function to read PDFs from GCS and extract text
 # MongoDB setup
 mongo_client = pymongo.MongoClient("mongodb://dev:N47309HxFWE2Ehc@35.209.224.122:27017")
-db = mongo_client["ChatbotDB"]
+db = mongo_client["ChatbotDB-DEV"]
 collection = db['files']
 
 def read_pdf_from_gcs(bucket_name, blob_names, chatbot_id, version_id):
