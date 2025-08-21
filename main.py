64d27602813854070b926d39fd24fef35c846ba2
Akshay00008 <Akshay.sharma@algo8.ai>
2025-08-21 16:40:56 +0530

    5002

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-# from utility.new_routes import app
+from utility.new_routes import app
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5002, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002, debug=True)
 
 
-'''
-Author: Bramhesh Kumar Srivastava
-Version: 0.0.1
-Date: 2023-04-19
-'''
-# Import the 'application' function from the 'index' module of the 'v1' package
-from index import create_app
+# '''
+# Author: Akshay Sharma
+# Version: 0.0.1
+# Date: 2023-04-19
+# '''
+# # Import the 'application' function from the 'index' module of the 'v1' package
+# from index import create_app
 
-app = create_app()
+# app = create_app()
 
-app.run(host = '0.0.0.0', port=5000)
+# app.run(host = '0.0.0.0', port=5000)
 
 
 
